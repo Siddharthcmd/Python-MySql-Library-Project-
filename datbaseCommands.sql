688f49824532b701bcfbe674b8caad35cfc7da71
Siddharth Mishra <siddharthcmd@gmail.com>
2020-12-30 12:31:22 +0530

    added commads

---
+++ datbaseCommands.sql
@@ -0,0 +1,40 @@
+create database MainLibraryDatabase;
+use MainLibraryDatabase;
+
+CREATE TABLE PUBLISHER(
+	NAME VARCHAR (20) PRIMARY KEY,
+	PHONE varchar(10),
+	ADDRESS VARCHAR(20)
+    );
+
+create table Book (
+	book_id integer primary key,
+	title varchar(20),
+    author varchar(20),
+    publisher varchar(20),
+    year_of_publish integer,
+    foreign key (publisher) references PUBLISHER (NAME) on delete cascade
+    );
+    
+create table LibraryBranch(
+	branchId integer primary key,
+    branchName varchar(20),
+    branchAddress varchar(20)
+    );
+
+CREATE TABLE BOOK_COPIES(
+	NoOfCopies INTEGER,
+	bookId integer,
+	branchId integer, foreign key(branchId) REFERENCES LibraryBranch (branchId) ON DELETE
+	CASCADE, foreign key (bookId) REFERENCES Book (book_id) ON DELETE CASCADE,
+	PRIMARY KEY (bookId,branchId)
+    );
+
+CREATE TABLE BOOK_LENDING(
+	DATE_OUT date,
+	DUE_DATE date,
+	BOOK_ID integer, foreign key(BOOK_ID) REFERENCES Book (book_id) ON DELETE CASCADE,
+	BRANCH_ID integer, foreign key(BRANCH_ID) references LibraryBranch (branchId) ON DELETE CASCADE,
+	CARD_NO integer,
+	primary key(BOOK_ID,BRANCH_ID,CARD_NO)
+	);
